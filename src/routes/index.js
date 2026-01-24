6daff76e74131610c2e47921e07dac4a355b62bc
MobCat <mobcat99@gmail.com>
2026-01-24 18:36:53 +1100

    Delete src directory

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  health: require('./health'),
-  session: require('./session'),
-  account: require('./account'),
-  admin: require('./admin'),
-  assets: require('./assets'),
-  avatar: require('./avatar'),
-  server: require('./server'),
-};
--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  redis: require('./redis'),
-  storage: require('./storage'),
-  auth: require('./auth'),
-  assets: require('./assets'),
-};
